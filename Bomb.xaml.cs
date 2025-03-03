059e4a959b32f2cad835a0b602f2029d4257a7ee
Антон Гладков <ya.anton-glladkov02@yandex.ru>
2025-03-03 16:25:32 +0300

    Добавьте файлы проекта.

---
+++ Bomb.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StarWars
+{
+    /// <summary>
+    /// Логика взаимодействия для Bomb.xaml
+    /// </summary>
+    public partial class Bomb : UserControl
+    {
+        public bool IsBam = false;
+
+
+        public Bomb()
+        {
+            InitializeComponent();
+        }
+
+        public bool IsHit(Rectangle Gun)
+        {
+            double bombLeft = Canvas.GetLeft(this);
+            double bombTop = Canvas.GetTop(this);
+
+            double gunLeft = Canvas.GetLeft(Gun);
+            double gunTop = Canvas.GetTop(Gun);
+
+            bool overlapX = bombLeft < gunLeft + Gun.Width && bombLeft + Width > gunLeft;
+
+            bool overlapY = bombTop < gunTop + Gun.Height && bombTop + Height > gunTop;
+
+            return overlapX && overlapY;
+        }
+    }
+}
